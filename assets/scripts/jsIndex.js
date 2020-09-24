b68b1bd74ce9e11a120a0098dde51fea40a26506
isfenixrojo <isfenixrojo@mail.com>
2020-09-24 16:48:29 -0500

    Se tiene una version sencilla del perfil de Simón, con un poco de CSS, Javascript

--- assets/scripts/jsIndex.js
+++ assets/scripts/jsIndex.js
@@ -1,1 +1,26 @@
-console.log("Estoy listo");
+let miImage = document.querySelector('img');
+miImage.onclick = function() {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'assets/images/simon.jpg') {
+        miImage.setAttribute('src', 'assets/images/animal.jpg');
+    } else {
+        miImage.setAttribute('src', 'assets/images/simon.jpg');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector('h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if (!miNombre) {
+        estableceNombreUsuario();
+    } else {
+        localStorage.setItem('nombre', miNombre);
+        miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+    }
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
